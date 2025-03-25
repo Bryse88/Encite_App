1215a0a68d06dee384bc02ccd2f79708900faa6d
bryse88 <bryseTurner0130@gmail.com>
2025-03-25 13:06:47 -0500

    Updating

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:encite/components/LoginComponents/Authentication/auth_wrapper.dart';
 import 'package:encite/firebase_options.dart';
-import 'package:encite/pages/app_navigator.dart';
-import 'package:encite/pages/login.dart';
-import 'package:encite/pages/proflie.dart';
-import 'package:encite/pages/scheduler.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,7 +29,7 @@ class MyApp extends StatelessWidget {
         ),
         fontFamily: 'SF Pro Display',
       ),
-      home: AuthWrapper(), // ← instead of just HomePage
+      home: const AuthWrapper(), // ← instead of just HomePage
       debugShowCheckedModeBanner: false,
     );
   }
